c739001b99170209966b99d36b158a8429f89375
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-04-15 22:58:09 +0300

    Écriture d'un bean CDI GestionnaireCompte : Transfert d'argent d'un compte à l'autre : Traiter les erreurs et messages de succès

--- src/main/java/fr/mendrika/tpbanquemendrika/jsf/Transfert.java
+++ src/main/java/fr/mendrika/tpbanquemendrika/jsf/Transfert.java
@@ -6,6 +6,7 @@ package fr.mendrika.tpbanquemendrika.jsf;
 
 import fr.mendrika.tpbanquemendrika.entity.CompteBancaire;
 import fr.mendrika.tpbanquemendrika.service.GestionnaireCompte;
+import fr.mendrika.tpbanquemendrika.util.Util;
 import jakarta.inject.Named;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
@@ -57,9 +58,36 @@ public class Transfert implements Serializable {
     }
     
     public String transfererArgent(){
+        boolean erreur = false;
         CompteBancaire source = gestionnaireCompte.findById(idSource);
+        if (source == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (source.getSolde() < montant) {
+                Util.messageErreur("Solde insuffisant !", "Solde insuffisant !", "form:montant");
+                erreur = true;
+            }
+        }
         CompteBancaire destination = gestionnaireCompte.findById(idDestination);
+        if (destination == null) {
+            Util.messageErreur("Aucun compte avec cet id!", "Aucun compte avec cet id !", "form:destination");
+            erreur = true;
+        }
+        
+        if (montant <= 0) {
+            Util.messageErreur("Veuillez insérez un montant positif !", "Veuillez insérez un montant positif !", "form:montant");
+            erreur = true;
+        }
+
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
         gestionnaireCompte.transfererArgent(source, destination, montant);
+        Util.addFlashInfoMessage("Transfert de "+montant+" Ar depuis "+source.getNom()+" vers "+destination.getNom()+" réussi!");
         return "listeComptes?faces-redirect=true";
     }
 }
